c2713ffe77ce73bc43daf832f6190841dbcb6dba
OleksiiKorgi <49100107+OleksiiKorgi@users.noreply.github.com>
2019-09-14 16:51:52 +0300

    Создание const длины, высоты и позиции облака

--- js/stat.js
+++ js/stat.js
@@ -1,10 +1,14 @@
 'use strict';
 
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+
 window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
+  ctx.fillRect(110, 20, CLOUD_WIDTH, CLOUD_HEIGHT);
 
   ctx.fillStyle = 'rgb(255,255,255)';
-  ctx.fillRect(100, 10, 420, 270);
-
+  ctx.fillRect(100, 10, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
