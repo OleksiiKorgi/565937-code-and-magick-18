d05287f8b2e32e7dd23bc988826d131f7d508cad
OleksiiKorgi <49100107+OleksiiKorgi@users.noreply.github.com>
2019-09-14 15:45:09 +0300

    Создание облака-фона для вывода сообщения и статистики

--- js/stat.js
+++ js/stat.js
@@ -1,4 +1,10 @@
 'use strict';
 
-var canvas = document.getElementByName('canvas');
-var ctx = canvas.getContext('2d');
+window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+
+  ctx.fillStyle = 'rgb(255,255,255)';
+  ctx.fillRect(100, 10, 420, 270);
+
+};
