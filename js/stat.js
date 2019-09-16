dc71af0577ae3ee31520d6e5ddd4df6606c3cb2c
OleksiiKorgi <49100107+OleksiiKorgi@users.noreply.github.com>
2019-09-16 11:35:09 +0300

    Вывод облака и текста заголовка на нём

--- js/stat.js
+++ js/stat.js
@@ -4,11 +4,25 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
+var GAP = 10;
+var FONT_GAP = 20;
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
 
 window.renderStatistics = function (ctx, names, times) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, CLOUD_WIDTH, CLOUD_HEIGHT);
 
-  ctx.fillStyle = 'rgb(255,255,255)';
-  ctx.fillRect(100, 10, CLOUD_WIDTH, CLOUD_HEIGHT);
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
+
+  ctx.font = '16px PT Mono';
+  ctx.fillStyle = 'black';
+  ctx.fillText('Ура вы победили!', CLOUD_X + FONT_GAP * 2, CLOUD_Y + GAP + FONT_GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + FONT_GAP * 2, CLOUD_Y + GAP + FONT_GAP * 2);
+
+  ctx.fillText('Вы')
+
+
 };
